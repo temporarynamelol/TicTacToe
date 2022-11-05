301895439825d8313e0f10b457b3238320ab5db8
David <David-williams-1996@outlook.com>
2022-11-05 16:12:53 +1100

    Create and test player factory

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const gameboard = (() => {
     const board = [];
 
-    return {board}
-})()
+    return {board};
+})();
+
+const player = (name) => {
+    return {name};
+}
+
+const person = player("David"); 
+
+console.log(person.name);
