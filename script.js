c2a661d1aa008d9cf8ae5e83a752865eb2ec6233
David <David-williams-1996@outlook.com>
2022-11-06 14:16:06 +1100

    Add turn logic to game and turn var to player factory

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const gameBoard = (() => {
 })();
 
 const player = (name) => {
-    return {name};
+    let turn;
+    let turns = 0;
+    return {name, turn, turns};
 }
+
+const player1 = player("player1");
+player1.turn = true;
+
+const player2 = player("player2");
 
 
 const gamePlay = (() => {
+
     let _squares = document.querySelectorAll(".square");
 
     const _populate = () => {
@@ -28,12 +36,23 @@ const gamePlay = (() => {
             square.addEventListener("click", () => {
                 const index = square.id;
                 if(gameBoard.board[index] <= 1) {
-                    gameBoard.board[index].push("X");
+                    if(player1.turn) {
+                        gameBoard.board[index].push("X");
+                        player1.turn = false;
+                        player2.turn = true;
+                    } else if (player2.turn) {
+                        gameBoard.board[index].push("O");
+                        player1.turn = true;
+                        player2.turn = false;
+                    }
+
                 }
                 _populate();
-            });
+            }, {once: true});
         })
+
     }
+
 
     return {playerInput}
     
